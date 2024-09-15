4553de44dce5d3b454ba3a8ae70e141e0f4dd5bd
KinPa01 <autodonut.2548@gmail.com>
2024-09-15 19:28:10 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -19,14 +19,14 @@ export default function App() {
     <div>
       <h1>Level {level}</h1>
       <img
-        src={level > 100 ? "src/img/kamo.webp" : "src/img/458986437_995054215966648_453887196021994011_n.jpg"}
+        src={level > 100 ? "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcS0XaKGoM0v-XFNwXyjghhkCWZmy3n0CgQfKD7jSGQyA-G5o6gB" : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcROOIlf6x5BnHNhAzO0QUZ2xocTJ2KNTqrM5kiKJHNqNhz2qutA"}
         alt="หมูเด้ง"
         style={{ width: size, height: size }}
       />
       <div>
-          <img src="src/img/TM.jpg" alt="แตงโม" width={50} height={50} onClick={() => feedPig(5)}/>
-          <img src="src/img/FT.webp" alt="ฟักทอง" width={50} height={50} onClick={() => feedPig(10)}/>
-          <img src="src/img/sqaspdi8.png" alt="หญ้า" width={50} height={50} onClick={() => feedPig(20)}/>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjhhFy1M9W9kAVb47VWXK2v23J5aczKBgGMebyC1meVh8lB1Gh" alt="แตงโม" width={50} height={50} onClick={() => feedPig(5)}/>
+          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTrp3kwuUXkxxpMBW0xOYHg-qT9vZZ8HCt1edxoBGnrMtjALDdM" alt="ฟักทอง" width={50} height={50} onClick={() => feedPig(10)}/>
+          <img src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTB08tE89Nhf7VVqwSOXnhmpjUAG7zNSfYiRUu_E0yypOo8d6CF" alt="หญ้า" width={50} height={50} onClick={() => feedPig(20)}/>
       </div>
       <button onClick={resetGame}>รีเซ็ต</button>
     </div>
